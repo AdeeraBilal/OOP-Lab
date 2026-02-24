9bc1743023eb8edc94ebf0877ff9e09395289a9f
Adeera Bilal <adeerabilal7@gmail.com>
2026-02-24 14:19:10 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,142 @@
+﻿            using System;
+            using System.IO;
+
+namespace Sign_in_and_sign_up_for_application
+    {
+        internal class Program
+        {
+            static string filePath = "users.txt";
+
+            static void Main(string[] args)
+            {
+                InitializeFile();
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("===== MAIN MENU =====");
+                    Console.WriteLine("1. Sign Up");
+                    Console.WriteLine("2. Sign In");
+                    Console.WriteLine("3. Exit");
+                    Console.Write("Enter choice: ");
+
+                    int choice = int.Parse(Console.ReadLine());
+
+                    if (choice == 1)
+                        SignUp();
+                    else if (choice == 2)
+                        SignIn();
+                    else
+                        break;
+
+                    Console.WriteLine("\nPress Enter to continue...");
+                    Console.ReadLine();
+                }
+            }
+
+            // ---------------- FILE INIT ----------------
+            static void InitializeFile()
+            {
+                if (!File.Exists(filePath))
+                    File.Create(filePath).Close();
+            }
+
+            // ---------------- SIGN UP ----------------
+            static void SignUp()
+            {
+                Console.Clear();
+                Console.WriteLine("===== SIGN UP =====");
+
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+
+                if (UserExists(username))
+                {
+                    Console.WriteLine("\nUser already exists!");
+                    return;
+                }
+
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                Console.Write("Role (owner/customer/supplier): ");
+                string role = Console.ReadLine();
+
+                File.AppendAllText(filePath, $"{username} {password} {role}\n");
+                Console.WriteLine("\nAccount created successfully!");
+            }
+
+            // ---------------- SIGN IN ----------------
+            static void SignIn()
+            {
+                Console.Clear();
+                Console.WriteLine("===== SIGN IN =====");
+
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                Console.Write("Role: ");
+                string role = Console.ReadLine();
+
+                if (Authenticate(username, password, role))
+                {
+                    Console.WriteLine("\nLogin successful!");
+
+                    if (role == "owner")
+                        OwnerMenu();
+                    else if (role == "customer")
+                        CustomerMenu();
+                    else
+                        SupplierMenu();
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid login details!");
+                }
+            }
+
+            // ---------------- CHECK USER ----------------
+            static bool UserExists(string username)
+            {
+                foreach (string line in File.ReadAllLines(filePath))
+                {
+                    string[] data = line.Split(' ');
+                    if (data[0] == username)
+                        return true;
+                }
+                return false;
+            }
+
+            // ---------------- AUTH ----------------
+            static bool Authenticate(string u, string p, string r)
+            {
+                foreach (string line in File.ReadAllLines(filePath))
+                {
+                    string[] data = line.Split(' ');
+                    if (data[0] == u && data[1] == p && data[2] == r)
+                        return true;
+                }
+                return false;
+            }
+
+            // ---------------- MENUS ----------------
+            static void OwnerMenu()
+            {
+                Console.WriteLine("\n--- OWNER MENU ---");
+            }
+
+            static void CustomerMenu()
+            {
+                Console.WriteLine("\n--- CUSTOMER MENU ---");
+            }
+
+            static void SupplierMenu()
+            {
+                Console.WriteLine("\n--- SUPPLIER MENU ---");
+            }
+        }
+    }
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sign in and sign up for application")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sign in and sign up for application")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("926783b9-e106-4cd8-94bb-193b01e2a107")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
